54844419d5a84bcc37982c6fee1b74fda3d43abb
YahirRadilla <yahirradi09@gmail.com>
2025-05-05 16:36:37 -0700

    blank project

---
+++ components/ui/Icons.tsx
@@ -0,0 +1,16 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+
+
+
+export const CircleInfoIcon = (props: any) => (
+    <FontAwesome6 name="circle-info" size={24} color="white" {...props} />
+);
+
+export const HomeIcon = (props: any) => (
+    <FontAwesome name="home" size={32} color="white" {...props} />
+);
+
+export const InfoIcon = (props: any) => (
+    <FontAwesome name="info" size={32} color="white" {...props} />
+);
