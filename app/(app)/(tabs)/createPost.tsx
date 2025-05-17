c1b6b9b006b28f878b2f10336028e7615c9bdd50
YahirRadilla <yahirradi09@gmail.com>
2025-05-16 18:52:34 -0700

    create post view file

---
+++ app/(app)/(tabs)/createPost.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function createPost() {
+    return (
+        <View>
+            <Text>createPost</Text>
+        </View>
+    )
+}
