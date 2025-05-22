78377b3ff7562e5a0c9acb914567b0f05cb4ea43
YahirRadilla <yahirradi09@gmail.com>
2025-05-22 04:05:45 -0700

    a

--- app/workspace/tabs/posts.tsx
+++ app/workspace/tabs/posts.tsx
@@ -1,4 +1,5 @@
 import { usePaginatedPostsByProvider } from "@/features/posts/usePaginatedPostsByProvider";
+import { FloatingActionButton } from "@/shared/components/FloatingActionButton";
 import { PostItemCard } from "@/shared/components/PostItemCard";
 import { useRouter } from "expo-router";
 import LottieView from "lottie-react-native";
@@ -29,55 +30,68 @@ export const PostsTab = () => {
         });
     };
 
+    const handlePressFloating = () => {
+        router.push("/post/createPost");
+    };
+
     return (
-        <FlatList
-            data={posts}
-            onEndReached={hasMore ? loadMore : undefined}
-            showsVerticalScrollIndicator={false}
-            ListHeaderComponent={
-                <View className="py-6">
-                    <Text className="text-white/90 font-bold ml-2 text-base">
-                        {posts.length} Publicaciones
-                    </Text>
-                    <Text className="text-white text-2xl font-bold ml-2">
-                        Publicaciones
-                    </Text>
-                </View>
-            }
-            renderItem={({ item, index }) => (
-                <Animatable.View
-                    animation="fadeInUp"
-                    duration={400}
-                    delay={index * 100}
-                    useNativeDriver
-                >
-                    <PostItemCard
-                        onPress={() => handleTouchPost(item.id)}
-                        image={item.images[0]}
-                        title={item.title}
-                        neighborhood={item.address.neighborhood}
-                        provider={item.provider.name}
-                        service={item.service}
-                        rate={item.valoration}
-                    />
-                </Animatable.View>
-            )}
-            keyExtractor={(item) => item.id}
-            onEndReachedThreshold={0.1}
-            refreshing={isRefreshing}
-            onRefresh={refresh}
-            ListFooterComponent={
-                loading && posts.length > 0 ? (
-                    <View className="w-full py-6 items-center justify-center">
-                        <LottieView
-                            source={require("@/assets/animations/loading.json")}
-                            autoPlay
-                            loop
-                            style={{ width: 80, height: 80 }}
-                        />
+
+        <View>
+            <FloatingActionButton />
+            <FlatList
+                data={posts}
+                onEndReached={hasMore ? loadMore : undefined}
+                showsVerticalScrollIndicator={false}
+                ListHeaderComponent={
+                    <View className="py-6">
+                        <Text className="text-white/90 font-bold ml-2 text-base">
+                            {posts.length} Publicaciones
+                        </Text>
+                        <Text className="text-white text-2xl font-bold ml-2">
+                            Publicaciones
+                        </Text>
+
                     </View>
-                ) : null
-            }
-        />
+                }
+                renderItem={({ item, index }) => (
+
+
+                    <Animatable.View
+                        animation="fadeInUp"
+                        duration={400}
+                        delay={index * 100}
+                        useNativeDriver
+                    >
+                        <PostItemCard
+                            onPress={() => handleTouchPost(item.id)}
+                            image={item.images[0]}
+                            title={item.title}
+                            neighborhood={item.address.neighborhood}
+                            provider={item.provider.name}
+                            service={item.service}
+                            rate={item.valoration}
+                        />
+                    </Animatable.View>
+
+                )}
+                keyExtractor={(item) => item.id}
+                onEndReachedThreshold={0.1}
+                refreshing={isRefreshing}
+                onRefresh={refresh}
+                ListFooterComponent={
+
+                    loading && posts.length > 0 ? (
+                        <View className="w-full py-6 items-center justify-center">
+                            <LottieView
+                                source={require("@/assets/animations/loading.json")}
+                                autoPlay
+                                loop
+                                style={{ width: 80, height: 80 }}
+                            />
+                        </View>
+                    ) : null
+                }
+            />
+        </View>
     );
 };
